3427bed89554ff758624b186dcd8e0028289fcf1
Soliyev909 <soliyev909@gmail.com>
2023-03-01 23:17:42 +0500

    Page

--- lib/page/view_page.dart
+++ lib/page/view_page.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:vocabulary/model/vocabulary_model.dart';
+import 'package:vocabulary/page/serivc_page.dart';
+import 'package:vocabulary/page/update_page.dart';
 
 import '../database/vocabulary_database.dart';
 import '../services/io_service.dart';
@@ -23,43 +27,13 @@ ViewPage(){  viewVocabulary();  }
           print('Wrong action! 🤣');
           viewVocabulary();
        }
-
-       serivc(command);
+       if(command<=vocabulary.length){
+         ServicPage().serivc(command);
+       }
 
   }
 
-     void serivc(int commond){
-        print("\n\n         Serice!\n");
-        print('👉 ${vocabulary[commond-1].translation} - ${vocabulary[commond-1].word}');
-        print('1 -> Update');
-        print('2 -> Delate');
-        print('3 -> Back');
-        print('0 -> Exit');
-        io.console('enter <= ');
-        int chus = io.number;
 
-
-         if(chus == 1){
-           print('\n\n         Update. 👨‍🔧\n');
-           io.console('Enter word <= ');
-           vocabulary[commond-1].word = io.text;
-           io.console('Enter translation <= ');
-           vocabulary[commond-1].translation = io.text;
-           viewVocabulary();
-         }
-         else if(chus == 2){
-           vocabulary.removeAt(commond-1);
-           ViewPage();
-         }
-         else if(chus == 3){
-           ViewPage();
-         }else if(chus == 0){
-           print('\n\n\n Program is over. 🤦');
-         }
-         else{
-           serivc(commond);
-         }
-     }
 
 
 
