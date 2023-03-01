39e4f2fc69c2043e313c20e58f87c9b5cd62a05b
jamshidhusenov <jamshidcikhusenov@gmail.com>
2023-03-01 13:09:04 +0500

    Initial commit

---
+++ bin/vocabulary.dart
@@ -0,0 +1,5 @@
+import 'package:vocabulary/vocabulary.dart' as vocabulary;
+
+void main(List<String> arguments) {
+  print('Hello world: ${vocabulary.calculate()}!');
+}
---
+++ test/vocabulary_test.dart
@@ -0,0 +1,8 @@
+import 'package:vocabulary/vocabulary.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
