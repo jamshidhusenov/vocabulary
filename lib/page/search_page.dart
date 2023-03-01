25b2b6b1099c4336baba362db4ba13a8d4578e7f
diyorbekshokirov <shokirovdiyorbek629@gmail.com>
2023-03-01 23:44:33 +0300

    Vocabularys O'gartirishlar kiritildi

--- lib/page/search_page.dart
+++ lib/page/search_page.dart
@@ -1,40 +1,44 @@
+import 'dart:io';
 import '../database/vocabulary_database.dart';
 import '../services/io_service.dart';
 import 'home_page.dart';
 
 
 class SearchPage{
-SearchPage(){
-  searchVocabulary();
-}
+
+SearchPage(){   searchVocabulary();    }
 
 void searchVocabulary() {
-  print('\n\n    Search - Qidiruv: ');
-  io.console('Enter text->');
-  String search = io.text;
-  int count = 0;
-  for (var element in vocabulary) {
-    if (element.word == search || element.translation == search) {
-      print('${element.word}-${element.translation}\n');
-      count++;
+
+     print('\n\n            Search - Qidiruv:\n ');
+     io.console(' Enter text: ');
+     String search = io.text;
+
+        int count = 0;
+        for (int i=0; i < vocabulary.length; i++) {
+
+           if ( vocabulary[i].word == search || vocabulary[i].translation == search ){
+               print(" ${ vocabulary[i].word } - ${vocabulary[i].translation }");
+               count++;
+           }
+        }
+
+           if (count == 0) {
+               print(' Vocabulary is not found 🤷‍♂️');
+           }
+
+              print('\n  1 -> Do you want to search again? \n  2 -> Back \n  0 -> Exit!\n ');
+              int num = io.number;
+
+               if (num == 0) {
+                     print(' \n\n\n  Program is over 🤦');
+               }
+               else if (num == 2) {
+                     HomePage();
+               }
+               else{
+                     searchVocabulary();
+               }
     }
-  }
-  if (count == 0) {
-    print('Vocabulary is not found 🤷‍♂️');
-  }
-  print('\n1 -> Do you want to search again?');
-  print('2 -> Back');
-  print('0 -> Exit!');
-  io.console('Enter ->');
-  int num = io.number;
-  if (num == 0) {
-    print('Program is over 🤦');
-  }
-  else if (num == 2) {
-    HomePage();
-  }else{
-    searchVocabulary();
-  }
-}
 
 }
--- lib/page/view_page.dart
+++ lib/page/view_page.dart
@@ -1,65 +1,65 @@
 import 'package:vocabulary/model/vocabulary_model.dart';
-
 import '../database/vocabulary_database.dart';
 import '../services/io_service.dart';
 import 'home_page.dart';
 
 class ViewPage{
-ViewPage(){  viewVocabulary();  }
+  ViewPage(){
+    viewVocabulary();
+}
 
   void viewVocabulary(){
-    print("\n\n      Vocabularys - Lug'atlar  \n");
+     print("            Vocabularys - Lug'atlar  \n");
 
-     for(int i=0; i < vocabulary.length; i++){
-       if(i == 0) print('0-> Back');
-       print('${i+1}-> ${vocabulary[i].translation} - ${vocabulary[i].word}');
-
-     }
-       io.console('Entr command <- ');
-       int command = io.number;
-       if(command == 0) HomePage();
-
-       if(command > vocabulary.length ){
-          print('Wrong action! 🤣');
-          viewVocabulary();
-       }
-
-       serivc(command);
-
+      for(int i=0; i < vocabulary.length; i++){
+         if (i == 0) {
+           print('  0 -> Back');
+         }
+            print('  ${i + 1} -> ${ vocabulary[i].translation } - ${ vocabulary[i].word }');
+      }
+         print('');
+         int command = io.number;
+         if(command == 0){
+            HomePage();
+         }
+           if(command > vocabulary.length ){
+              print('Wrong action! 🤣');
+              viewVocabulary();
+           }
+                serivc(command);
   }
 
-     void serivc(int commond){
-        print("\n\n         Serice!\n");
-        print('👉 ${vocabulary[commond-1].translation} - ${vocabulary[commond-1].word}');
-        print('1 -> Update');
-        print('2 -> Delate');
-        print('3 -> Back');
-        print('0 -> Exit');
-        io.console('enter <= ');
-        int chus = io.number;
+
+         void serivc( int commond ){
+
+              print("\n\n               Service\n");
+              print('  ${vocabulary[commond-1].translation} - ${vocabulary[commond-1].word}');
+              print('  \n  1 -> Update \n  2 -> Delate \n  3 -> Back \n  0 -> Exit\n');
+              int chus = io.number;
 
 
-         if(chus == 1){
-           print('\n\n         Update. 👨‍🔧\n');
-           io.console('Enter word <= ');
-           vocabulary[commond-1].word = io.text;
-           io.console('Enter translation <= ');
-           vocabulary[commond-1].translation = io.text;
-           viewVocabulary();
-         }
-         else if(chus == 2){
-           vocabulary.removeAt(commond-1);
-           ViewPage();
-         }
-         else if(chus == 3){
-           ViewPage();
-         }else if(chus == 0){
-           print('\n\n\n Program is over. 🤦');
-         }
-         else{
-           serivc(commond);
-         }
-     }
+                    if(chus == 1){
+                       print('\n\n             Update. 👨‍🔧\n');
+                       io.console(' Enter word: ');
+                       vocabulary[commond-1].word = io.text;
+                       io.console(' Enter translation: ');
+                       vocabulary[commond-1].translation = io.text;
+                       viewVocabulary();
+                    }
+                     else if(chus == 2){
+                         vocabulary.removeAt(commond-1);
+                         ViewPage();
+                     }
+                     else if(chus == 3){
+                         ViewPage();
+                     }
+                     else if(chus == 0){
+                         print('\n\n\n Program is over. 🤦');
+                     }
+                    else{
+                        serivc(commond);
+                    }
+        }
 
 
 
